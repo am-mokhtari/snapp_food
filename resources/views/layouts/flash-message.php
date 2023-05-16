4e8ed6045fc0b9517235d27f28fad56dc7ee50a5
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-05-16 22:35:25 +0330

    handle errors by flash messages

--- resources/views/layouts/flash-message.php
+++ resources/views/layouts/flash-message.php
@@ -1,25 +1,39 @@
 <?php
-$hasSession = false;
-if (Session::has('success')) {
-    $colors = 'bg-lime-200 text-lime-600';
-    $message = Session::get('success');
-    $hasSession = true;
-} elseif (Session::has('warning')) {
-    $colors = 'bg-yellow-200 text-yellow-700';
-    $message = Session::get('warning');
-    $hasSession = true;
-} elseif (Session::has('danger')) {
-    $colors = 'bg-red-200 text-red-700';
-    $message = Session::get('danger');
-    $hasSession = true;
-} elseif (Session::has('message')) {
-    $colors = 'bg-teal-100 text-teal-600';
-    $message = Session::get('message');
-    $hasSession = true;
-}
-
-if ($hasSession) { ?>
-    <div class="p-2 m-2 <?= $colors ?> border rounded">
-        <h2 class="h2"><?= $message ?></h2>
+if (Session::has('success')) { ?>
+    <div class="p-2 m-2 bg-lime-200 text-lime-600 border rounded">
+        <h2 class="h2"><?= Session::get('success') ?></h2>
     </div>
-<?php } ?>
+    <?php
+}
+if (Session::has('warning')) { ?>
+    <div class="p-2 m-2 bg-yellow-200 text-yellow-700 border rounded">
+        <h2 class="h2"><?= Session::get('warning') ?></h2>
+    </div>
+    <?php
+}
+if (Session::has('danger')) { ?>
+    <div class="p-2 m-2 bg-red-200 text-red-700 border rounded">
+        <h2 class="h2"><?= Session::get('danger') ?></h2>
+    </div>
+    <?php
+}
+if (Session::has('message')) { ?>
+    <div class="p-2 m-2 bg-teal-100 text-teal-600 border rounded">
+        <h2 class="h2"><?= Session::get('message') ?></h2>
+    </div>
+    <?php
+}
+if ($errors->any()) {
+    foreach ($errors->all() as $error) {
+        ?>
+        <div class="p-2 m-2 bg-red-200 text-red-700 border rounded">
+            <h2 class="h2">
+                <?php
+                echo $error;
+                ?>
+            </h2>
+        </div>
+        <?php
+    }
+}
+?>
