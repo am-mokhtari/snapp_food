0b50e564e5ba125f79e48b44523712134aacab71
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-05-16 22:33:09 +0330

    add custom rule for phone number

--- app/Http/Requests/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Auth;
 
 use App\Models\User;
+use App\Rules\PhoneNumber;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
 
@@ -26,25 +27,8 @@ class RegisterUserRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255', 'min:3'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
-            'phone_number' => ['required', 'numeric', 'unique:' . User::class, 'regex:/^(09|989|9)\d{9}/i'],
+            'phone_number' => ['required', 'numeric', 'unique:' . User::class, new PhoneNumber],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ];
-    }
-
-    public function messages(): array
-    {
-        return [
-            'name.required' => 'Enter your name.',
-            'name.max' => 'Your name is too long.',
-            'name.min' => 'Your name is too short.',
-            'email.required' => 'Enter your email.',
-            'email.max' => 'Your email is too long.',
-            'email.unique' => 'This email is already exists.',
-            'phone_number.required' => 'Enter your phone number.',
-            'phone_number.numeric' => 'Phone number must be numeric.',
-            'phone_number.unique' => 'This phone number is already exists.',
-            'phone_number.regex' => 'This phone number is invalid.',
-            'password.required' => 'Enter password.',
         ];
     }
 }
---
+++ app/Rules/PhoneNumber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class PhoneNumber implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!preg_match('/^(09|989|9)\d{9}/i', $value)){
+            $fail('This :attribute is invalid.');
+        }
+    }
+}
