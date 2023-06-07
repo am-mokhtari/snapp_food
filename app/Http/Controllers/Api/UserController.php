e33519351471b62aa9ddc3131f6fc9cbf0bb245f
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-06-08 01:24:27 +0330

    Authentication for apis

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\ApiRegisterRequest;
+use App\Models\User;
+use App\Rules\PhoneNumber;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Sanctum;
+
+class UserController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $username = [];
+        if (is_numeric($request->username)) {
+            $request->username = '0' . strrev((str_split(strrev($request->username), 10))[0]);
+            $username = ['phone_number' => $request->username, 'password' => $request->password];
+        } elseif (preg_match('/^[a-zA-Z].{3,20}@.{3,10}\.[a-zA-Z]{2,10}$/i', $request->username)) {
+            $username = ['email' => $request->username, 'password' => $request->password];
+        }
+
+        if (Auth::attempt($username)) {
+            $token = $request->user()->createToken("authenticated");
+            return response()->json([
+                'msg' => "You are signed in. Your Token is: " . $token->plainTextToken
+            ]);
+        } else {
+            return response()->json([
+                "msg" => "username or password is wrong."
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if (isset($request->phone_number)) {
+            $request->phone_number = strrev((str_split(strrev($request->phone_number), 10))[0]);
+        }
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'min:3'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'phone_number' => ['required', 'numeric', 'unique:' . User::class, new PhoneNumber],
+            'password' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password),
+            'role' => 'customer',
+        ]);
+
+        $token = $user->createToken("authenticated");
+        return response()->json([
+            "msg" => "You are signed up successfully. Your Token is: " . $token->plainTextToken . ". Dont Forget It."
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        if ($token = $request->bearerToken()) {
+            $model = Sanctum::$personalAccessTokenModel;
+            $accessToken = $model::findToken($token);
+            $accessToken->delete();
+        }
+
+        return new Response('', 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('auth:sanctum')->group(function (){
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-//  addresses apis
+    //  addresses apis
     Route::get("/addresses", [\App\Http\Controllers\Api\AddressController::class, 'show']);
     Route::post("/addresses", [\App\Http\Controllers\Api\AddressController::class, 'store']);
     Route::post("/addresses/{address_id}", [\App\Http\Controllers\Api\AddressController::class, 'update']);
 });
+
+//  authenticate apis
+Route::post('/login', [\App\Http\Controllers\Api\UserController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'register']);
+Route::post('/logout', [\App\Http\Controllers\Api\UserController::class, 'logout']);
