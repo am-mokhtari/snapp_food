b4a46377372f960a8d0a6d2a89db8a24f122cd19
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-13 16:58:58 +0330

    add order model

---
+++ app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'cart_id',
+        'amount',
+        'status',
+        'tracking_code',
+        'score',
+    ];
+
+    protected $hidden = [
+        'cart_id'
+    ];
+}
