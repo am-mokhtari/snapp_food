3b7a466c90590863c3a25b0a7deec5397d70299a
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-13 16:58:30 +0330

    add cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'user_id',
+        'discount_id',
+        'is_closed',
+        'restaurant_id'
+    ];
+
+    protected $hidden = [
+        'user_id',
+        'is_closed',
+        'restaurant_id'
+    ];
+
+    public function foods(): BelongsToMany
+    {
+        return $this->belongsToMany(Food::class, 'cart_items');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+}
