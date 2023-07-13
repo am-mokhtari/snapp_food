ef7e32cae9793114b1feac668e08b9f64815e03e
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-13 17:02:24 +0330

    add migrations of carts & orders & cart-items & comments

---
+++ database/migrations/2023_05_17_173947_create_carts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->noActionOnDelete();
+            $table->foreignId('restaurant_id')
+                ->constrained('restaurants')
+                ->noActionOnDelete();
+            $table->foreignId('discount_id')
+                ->nullable()
+                ->constrained('discount_codes')
+                ->nullOnDelete();
+            $table->boolean('is_closed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart');
+    }
+};
---
+++ database/migrations/2023_05_17_173959_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained('carts')
+            ->onUpdate('NO ACTION')
+            ->noActionOnDelete();
+            $table->integer('amount');
+            $table->enum('status', ['pending', 'preparing', 'sending', 'delivered', 'canceled']);
+            $table->string('tracking_code');
+            $table->integer('score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
