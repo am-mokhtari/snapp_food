4aa1f62510bac27e63e671068a6d4d2e74af78ea
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-13 17:44:26 +0330

    update saving restaurant info

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\Restaurant;
 use App\Models\RestaurantType;
 use App\Rules\PhoneNumber;
@@ -32,14 +33,34 @@ class SellerController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => ['regex:/(\w\s*){3,100}$/i'],
+            'type_id' => ['numeric', 'exists:'.RestaurantType::class.',id'],
+            'phone_number' => ['required', 'numeric', new PhoneNumber],
+            'address' => 'string|min:10|max:500|required',
+            'latitude' => 'numeric|between:-180,180|required',
+            'longitude' => 'numeric|between:-180,180|required',
+            'accountNumber' => ['regex:/(\d){10,20}$/i']
+        ]);
+
+        $address = new Address;
+        $address->title = $request->name . "'s address";
+        $address->address = $request->address;
+        $address->latitude = $request->latitude;
+        $address->longitude = $request->longitude;
+        $address->save();
+
         $info = new Restaurant;
         $info->name = $request->name;
         $info->user_id = Auth::user()->id;
         $info->type_id = $request->type_id;
         $info->phone_number = $request->phone_number;
-        $info->address = $request->address;
+        $info->address_id = $address->id;
         $info->account_number = $request->accountNumber;
         $info->save();
+
+        $address->restaurant_id  = $info->id;
+        $address->save();
 
         Session::flash("success", "Restaurant Info Saved");
         return redirect('/dashboard/seller');
@@ -70,7 +91,9 @@ class SellerController extends Controller
             'name' => ['regex:/(\w\s*){3,100}$/i'],
             'type_id' => ['numeric', 'exists:'.RestaurantType::class.',id'],
             'phone_number' => ['required', 'numeric', new PhoneNumber],
-            'address' => ['regex:/(\w\s*){5,}$/i'],
+            'address' => 'string|min:10|max:500|required',
+            'latitude' => 'numeric|between:-180,180|required',
+            'longitude' => 'numeric|between:-180,180|required',
             'accountNumber' => ['regex:/(\d){10,20}$/i']
         ]);
 
