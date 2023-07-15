8fea43ba630c7dff1f4f4907fa3e31e572dc6a0d
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-16 02:54:12 +0330

    update RestaurantResource.php for fix bug

--- app/Http/Resources/RestaurantResource.php
+++ app/Http/Resources/RestaurantResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,7 +20,7 @@ class RestaurantResource extends JsonResource
             "name" => $this->name,
             "type" => $this->type->title,
             "phone_number" => $this->phone_number,
-            "address" => $this->address->only([
+            "address" => Address::find($this->address_id)->only([
                 'address',
                 'latitude',
                 'longitude',
