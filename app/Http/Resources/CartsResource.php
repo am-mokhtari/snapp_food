4ff5f23d5695f17d27e5f926336a3517193c8241
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-18 03:09:27 +0330

    update inputted values in CartsResource.php

--- app/Http/Resources/CartsResource.php
+++ app/Http/Resources/CartsResource.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Food;
-use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,10 +15,10 @@ class CartsResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this[0]['id'],
-            "items" => CartItemsResource::collection($this[1]),
-            "created_at" => $this[0]->created_at,
-            "updated_at" => $this[0]->updated_at,
+            "id" => $this['id'],
+            "items" => CartItemResource::collection($this['foods']),
+            "created_at" => $this['created_at'],
+            "updated_at" => $this['updated_at'],
         ];
     }
 }
