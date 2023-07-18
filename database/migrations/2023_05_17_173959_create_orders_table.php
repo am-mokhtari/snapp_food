e14d2c6fecf37fe10b542f94e54450b5587751b2
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-18 16:04:22 +0330

    update orders table(add user_id and set default on pending)

--- database/migrations/2023_05_17_173959_create_orders_table.php
+++ database/migrations/2023_05_17_173959_create_orders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('cart_id')->constrained('carts')
-            ->onUpdate('NO ACTION')
-            ->noActionOnDelete();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->noActionOnDelete();
+            $table->foreignId('cart_id')
+                ->constrained('carts')
+                ->noActionOnDelete();
             $table->integer('amount');
-            $table->enum('status', ['pending', 'preparing', 'sending', 'delivered', 'canceled']);
+            $table->enum('status', ['pending', 'preparing', 'sending', 'delivered', 'canceled'])
+                ->default('pending');
             $table->string('tracking_code');
             $table->integer('score')->nullable();
             $table->timestamps();
