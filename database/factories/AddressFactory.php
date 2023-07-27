ddb3f97894978b957423fe63361347c343d87ad7
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-27 03:38:29 +0330

    perf(AddressFactory): Changing selection method of the address owner
    used Arr helper for choose randomly between user_id or restaurant_id,
    instead of rand() method

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Restaurant;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
@@ -18,13 +19,13 @@ class AddressFactory extends Factory
      */
     public function definition(): array
     {
-        rand(0, 1) ? $type = ['user_id' => User::factory()] : $type = ['restaurant_id' => Restaurant::factory()];
-
         return array_merge([
             'title' => fake()->title(),
             'address' => fake()->address(),
             'latitude' => fake()->latitude(),
             'longitude' => fake()->longitude(),
-        ], $type);
+        ],
+            Arr::random([['user_id' => User::factory()], ['restaurant_id' => Restaurant::factory()]])
+        );
     }
 }
