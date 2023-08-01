2d4cc110095e244346167e55c4bea4646a807915
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-08-01 17:34:00 +0330

    style(api router): imported path of controllers instead write everywhere

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,35 @@
 <?php
 
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\FoodController;
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
     //  addresses apis
-    Route::get("/addresses", [\App\Http\Controllers\Api\AddressController::class, 'show']);
-    Route::post("/addresses", [\App\Http\Controllers\Api\AddressController::class, 'store']);
-    Route::post("/addresses/{address_id}", [\App\Http\Controllers\Api\AddressController::class, 'update']);
+    Route::get("/addresses", [AddressController::class, 'show']);
+    Route::post("/addresses", [AddressController::class, 'store']);
+    Route::post("/addresses/{address_id}", [AddressController::class, 'update']);
 
     //    user api
-    Route::patch("/user", [\App\Http\Controllers\Api\UserController::class, 'updateInfo']);
+    Route::patch("/user", [UserController::class, 'updateInfo']);
 
     //    restaurant apis
-    Route::get('/restaurants', [\App\Http\Controllers\Api\RestaurantController::class, 'index']);
-    Route::get('/restaurants/{restaurant_id}', [\App\Http\Controllers\Api\RestaurantController::class, 'show']);
+    Route::get('/restaurants', [RestaurantController::class, 'index']);
+    Route::get('/restaurants/{restaurant_id}', [RestaurantController::class, 'show']);
 
     //    Carts apis
     Route::prefix('/carts')
-        ->controller(\App\Http\Controllers\Api\CartController::class)
+        ->controller(CartController::class)
         ->group(function () {
             Route::get('/', 'index');
             Route::post('/add', 'store');
@@ -43,9 +40,9 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 //    Foods api
-    Route::get('/restaurants/{restaurant_id}/foods', [\App\Http\Controllers\Api\FoodController::class, 'show']);
+    Route::get('/restaurants/{restaurant_id}/foods', [FoodController::class, 'show']);
 
 //  authenticate apis
-    Route::post('/login', [\App\Http\Controllers\Api\UserController::class, 'login']);
-    Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'register']);
-    Route::post('/logout', [\App\Http\Controllers\Api\UserController::class, 'logout']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/logout', [UserController::class, 'logout']);
