4d08b2f13af52ff46b47a48876ba20bf5f87c8b2
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-08-02 21:34:26 +0330

    refactor DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,8 @@
 
 namespace Database\Seeders;
 
-
-use App\Models\Address;
-use App\Models\Cart;
 use App\Models\CartItem;
 use App\Models\Comment;
-use App\Models\DiscountCode;
-use App\Models\Food;
-use App\Models\FoodCategory;
-use App\Models\Order;
-use App\Models\Restaurant;
-use App\Models\RestaurantType;
 use App\Models\Transaction;
 use Illuminate\Database\Seeder;
 
@@ -23,35 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//      users:
-        \App\Models\User::factory(10)->create();
-
-//      restaurant_types
-        RestaurantType::factory(4)->create();
-
-//      restaurants
-        Restaurant::factory(6)->create();
-
-//      food_categories
-        FoodCategory::factory(5)->create();
-
-//      foods
-        Food::factory(15)->create();
-
-//      addresses
-        Address::factory(14)->create();
-
-//      discount_codes
-        DiscountCode::factory(3)->create();
-
-//      carts
-        Cart::factory(4)->create();
-
 //      cart_items
         CartItem::factory(10)->create();
-
-//      orders
-        Order::factory(3)->create();
 
 //      transactions
         Transaction::factory(4)->create();
