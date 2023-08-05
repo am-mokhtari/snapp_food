3062be0362622416e0670b73ad0966b6ae575ba1
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-08-06 01:31:57 +0330

    fix(api router): validation of parameter in getting comments api

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::post('/add', 'store');
             Route::patch('/add', 'update');
             Route::get('/{cart}', 'show')->whereNumber("cart");
-            Route::post('/{cart}/pay', 'pay')->whereNumber("cart_id");
+            Route::post('/{cart}/pay', 'pay')->whereNumber("cart");
         });
 
     //     Comments
