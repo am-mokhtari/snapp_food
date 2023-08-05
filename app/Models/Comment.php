09abd0d9337fe1d66c28c3fb630a3ac5cd0a13cc
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-08-06 01:50:05 +0330

    feat(Comment model): add relation func with order table

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -19,4 +20,9 @@ class Comment extends Model
         'order_id',
         'user_id',
     ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
